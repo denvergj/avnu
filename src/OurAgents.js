aec09ad3294b5c0db4ed1d54fff8ba63a015fa60
James <james@ljx.com.au>
2018-07-10 17:08:52 +1000

    My Changes , hopefully this doesnt break everything

--- src/OurAgents.js
+++ src/OurAgents.js
@@ -1,12 +1,12 @@
-import React, { Component, createElement } from 'react';
-import Helmet from 'react-helmet';
-import { Link } from 'react-router-dom';
-import { createClient } from 'contentful';
-import Hero from './Hero';
-import marksy from 'marksy';  
-import Slide from 'react-reveal/Slide';
-import VisibilitySensor from 'react-visibility-sensor';
-import InViewMonitor from 'react-inview-monitor';
+import React, { Component, createElement } from "react";
+import Helmet from "react-helmet";
+import { Link } from "react-router-dom";
+import { createClient } from "contentful";
+import Hero from "./Hero";
+import marksy from "marksy";
+import Slide from "react-reveal/Slide";
+import VisibilitySensor from "react-visibility-sensor";
+import InViewMonitor from "react-inview-monitor";
 
 const getMarkup = field => {
   if (!field) return null;
@@ -22,7 +22,7 @@ class OurAgents extends Component {
     super(props);
 
     this.state = {
-	  hero: null,
+      hero: null,
       agents: null,
       quotes: null
     };
@@ -34,11 +34,10 @@ class OurAgents extends Component {
       accessToken: process.env.REACT_APP_ACCESS_TOKEN
     });
 
-	
-      client
+    client
       // use getEntries because it does link resolution
       .getEntries({
-	    content_type: 'agentListPage'
+        content_type: "agentListPage"
       })
       .then(response => {
         // extract the data from the response array
@@ -51,12 +50,11 @@ class OurAgents extends Component {
         console.log(heroContent);
       })
       .catch(console.error);
-		
 
-      client
+    client
       // use getEntries because it does link resolution
       .getEntries({
-	    content_type: 'agent'
+        content_type: "agent"
       })
       .then(response => {
         // extract the data from the response array
@@ -66,16 +64,16 @@ class OurAgents extends Component {
         this.setState({
           agents: fields
         });
-         console.log("Agents:");
-         console.log(fields);
+        console.log("Agents:");
+        console.log(fields);
       })
       .catch(console.error);
-      
-      // Quotes
-      client
+
+    // Quotes
+    client
       // use getEntries because it does link resolution
       .getEntries({
-	    content_type: 'brandQuoteBlock'
+        content_type: "brandQuoteBlock"
       })
       .then(response => {
         // extract the data from the response array
@@ -90,125 +88,144 @@ class OurAgents extends Component {
       })
       .catch(console.error);
   }
-  
+
   componentDidMount() {
-	  document.body.classList.add('our-agents-page-content');
+    document.body.classList.add("our-agents-page-content");
   }
 
   render() {
     let mainTitle = null,
-    	introText = null,
-    	pageHeading = null,
-    	heroData = null,
-    	headline = null,
-    	heroImage = null;
-    
-    if(this.state.hero) {
-	  heroData = this.state.hero;
-	  mainTitle = heroData.heroImageBody;
-	  introText = heroData.heroImageHeading;
-	  heroImage = heroData.heroImage.fields.file.url;
-	  headline = heroData.pageHeading;
+      introText = null,
+      pageHeading = null,
+      heroData = null,
+      headline = null,
+      heroImage = null;
+
+    if (this.state.hero) {
+      heroData = this.state.hero;
+      mainTitle = heroData.heroImageBody;
+      introText = heroData.heroImageHeading;
+      heroImage = heroData.heroImage.fields.file.url;
+      headline = heroData.pageHeading;
     }
-    
+
     return (
-	    <div className="our-agents-page">
-	    	<Helmet title={"Avnu - " + headline} />
-			<Hero 
-				mainTitle={mainTitle}
-				introText={introText} 
-				imgSrc={heroImage}
-				icon="/images/man-sunrise.png"
-				headline={headline}
-			/>
-      		
-      		
-      	  <div className="three-tiles">
-	       	<div className="content-container">
-		      	<div className="row">
-			      	{this.state.agents && this.state.agents.map((agent, i) => { 
-				      	if(i < 3) {
-				      		return (
-						      	<div key={i} className="content">
-						      		<img src={(agent.fields.agentTile ? agent.fields.agentTile.fields.tileImage.fields.file.url : '')} />
-					      			<div className="info">
-					      				<h3>{agent.fields.firstName}</h3>
-					      				<p>{agent.fields.tileBody}</p>
-					      				<Link to={`/our-agents/${agent.fields.slug}/`} className="with-arrow">
-											See {agent.fields.firstName + "'s"} profile
-										</Link>
-					      			</div>
-					      		</div>
-				      		);
-			      		}
-					})}
-		      	</div>
-		    </div>
-	      </div>
-	      
-	    <InViewMonitor
-		  classNameNotInView='vis-hidden'
-		  classNameInView='animated slideInRight quote-area content-container'
-		>
-		   <div>
-		  		{this.state.quotes && this.state.quotes.map((quotes, i) => { 
-			      	if(i == 0) {
-			      		return (
-					      	<div key={i} className="the-quote">
-								<img src="/images/man-white.png" />
-								<p>{quotes.fields.quoteBody}</p>
-							</div>
-			      		);
-		      		}
-				})} 
-			</div>
-		</InViewMonitor>
-		
-		
-		<div className="three-tiles grey">
-			<div className="content-container">
-			  	<div className="row">
-			      	{this.state.agents && this.state.agents.map((agent, i) => { 
-				      	if(i > 2) {
-				      		return (
-						      	<div key={i} className="content">
-						      		<img src={(agent.fields.agentTile ? agent.fields.agentTile.fields.tileImage.fields.file.url : '')} />
-					      			<div className="info">
-					      				<h3>{agent.fields.firstName}</h3>
-					      				<p>{agent.fields.tileBody}</p>
-					      				<Link to={`/our-agents/${agent.fields.slug}/`} className="with-arrow">
-											See {agent.fields.firstName + "'s"} profile
-										</Link>
-					      			</div>
-					      		</div>
-				      		);
-			      		}
-					})}
-			  	</div>
-			</div>
-		</div>
-		
-		
-		<InViewMonitor
-		  classNameNotInView='vis-hidden'
-		  classNameInView='animated slideInRight quote-area content-container black'
-		>
-		  <div>
-		  		{this.state.quotes && this.state.quotes.map((quotes, i) => { 
-			      	if(i == 1) {
-			      		return (
-					      	<div key={i} className="the-quote">
-								<img src="/images/man-white.png" />
-								<p>{quotes.fields.quoteBody}</p>
-							</div>
-			      		);
-		      		}
-				})} 
-			</div>
-		</InViewMonitor>
-			
-      					
-		</div>
+      <div className="our-agents-page">
+        <Helmet title={"Avnu - " + headline} />
+        <Hero
+          mainTitle={mainTitle}
+          introText={introText}
+          imgSrc={heroImage}
+          icon="/images/man-sunrise.png"
+          headline={headline}
+        />
+
+        <div className="three-tiles">
+          <div className="content-container">
+            <div className="row">
+              {this.state.agents &&
+                this.state.agents.map((agent, i) => {
+                  if (i < 3) {
+                    return (
+                      <div key={i} className="content">
+                        <img
+                          src={
+                            agent.fields.agentTile
+                              ? agent.fields.agentTile.fields.tileImage.fields
+                                  .file.url
+                              : ""
+                          }
+                        />
+                        <div className="info">
+                          <h3>{agent.fields.firstName}</h3>
+                          <p>{agent.fields.tileBody}</p>
+                          <Link
+                            to={`/our-agents/${agent.fields.slug}/`}
+                            className="with-arrow"
+                          >
+                            See {agent.fields.firstName + "'s"} profile
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
+            </div>
+          </div>
+        </div>
+
+        <InViewMonitor
+          classNameNotInView="vis-hidden"
+          classNameInView="animated slideInRight quote-area content-container"
+        >
+          <div>
+            {this.state.quotes &&
+              this.state.quotes.map((quotes, i) => {
+                if (i == 0) {
+                  return (
+                    <div key={i} className="the-quote">
+                      <img src="/images/man-white.png" />
+                      <p>{quotes.fields.quoteBody}</p>
+                    </div>
+                  );
+                }
+              })}
+          </div>
+        </InViewMonitor>
+
+        <div className="three-tiles grey">
+          <div className="content-container">
+            <div className="row">
+              {this.state.agents &&
+                this.state.agents.map((agent, i) => {
+                  if (i > 2) {
+                    return (
+                      <div key={i} className="content">
+                        <img
+                          src={
+                            agent.fields.agentTile
+                              ? agent.fields.agentTile.fields.tileImage.fields
+                                  .file.url
+                              : ""
+                          }
+                        />
+                        <div className="info">
+                          <h3>{agent.fields.firstName}</h3>
+                          <p>{agent.fields.tileBody}</p>
+                          <Link
+                            to={`/our-agents/${agent.fields.slug}/`}
+                            className="with-arrow"
+                          >
+                            See {agent.fields.firstName + "'s"} profile
+                          </Link>
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
+            </div>
+          </div>
+        </div>
+
+        <InViewMonitor
+          classNameNotInView="vis-hidden"
+          classNameInView="animated slideInLeft quote-area content-container black"
+        >
+          <div>
+            {this.state.quotes &&
+              this.state.quotes.map((quotes, i) => {
+                if (i == 1) {
+                  return (
+                    <div key={i} className="the-quote quote-secondary">
+                      <img src="/images/man-white.png" />
+                      <p>{quotes.fields.quoteBody}</p>
+                    </div>
+                  );
+                }
+              })}
+          </div>
+        </InViewMonitor>
+      </div>
     );
   }
 }
--- src/StandardPage.js
+++ src/StandardPage.js
@@ -1,9 +1,10 @@
-import React, { Component, createElement } from 'react';
-import Helmet from 'react-helmet';
-import { createClient } from 'contentful';
-import Hero from './Hero';
-import Zoom from 'react-reveal/Zoom';
-import marksy from 'marksy'; 
+import React, { Component, createElement } from "react";
+import Helmet from "react-helmet";
+import { createClient } from "contentful";
+import Hero from "./Hero";
+import Zoom from "react-reveal/Zoom";
+import { withRouter } from "react-router-dom";
+import marksy from "marksy";
 
 const getMarkup = field => {
   if (!field) return null;
@@ -17,7 +18,6 @@ const getMarkup = field => {
 class StandardPage extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       data: null
     };
@@ -32,8 +32,8 @@ class StandardPage extends Component {
     client
       // use getEntries because it does link resolution
       .getEntries({
-	    content_type: 'aboutAvnuStandardContentPages',
-        'fields.menuItemText[in]': 'Our Story'
+        content_type: "aboutAvnuStandardContentPages",
+        "fields.menuItemText[in]": this.props.match.params.id
       })
       .then(response => {
         // extract the data from the response array
@@ -47,109 +47,56 @@ class StandardPage extends Component {
       })
       .catch(console.error);
   }
-  
+
   componentDidMount() {
-	  document.body.classList.add('standard-page-content');
+    document.body.classList.add("standard-page-content");
   }
 
   render() {
     let mainTitle = null,
-    	introText = null,
-    	pageHeading = null;
-		
-	 if (this.state.data) {
+      introText = null,
+      pageHeading = null;
+
+    if (this.state.data) {
       mainTitle = this.state.data.heroImageHeading;
       introText = this.state.data.heroImageBody;
       pageHeading = this.state.data.pageHeading;
     }
-    
-    return (
-	    <div className="standard-page">
-	    	<Helmet title="Avnu - Standard Page" />
-			<Hero 
-				mainTitle={mainTitle} 
-				introText={introText} 
-				imgSrc="/images/header.jpg" 
-				icon="/images/clock.svg"
-				headline={pageHeading}
-			/>
-			
-			{this.state.data && this.state.data.contentBlocks.map((block, i) => {   
-				 if(i < 1) {
-		       		 let blockBody = getMarkup(block.fields.blockBody);
-		             return (
-			             <Zoom key={i}>
-						 <section key={i} className="content double">
-							<div className="content-container flex">
-								<div>
-									<img src="/images/content-image.jpg" />
-								</div>
-								<div className="text">
-									{blockBody}
-								</div>
-							</div>
-						</section>
-						</Zoom>
-		            );             
-	            }
-	        })}
-	        <Zoom>
-				<div className="quote-area content-container">
-					<div className="the-quote">
-		      			<img src="https://images.ctfassets.net/dkcrc82u6zt9/GCYZPz8aAeo8Uw4miyIIo/06f487a622bb6d2d3681c87d0b3d1bd0/man.png" />
-		      			<p>"Time or brand related quote that relates to the brand."</p>
-		      		</div>
-	      		</div>
-      		</Zoom>
-      		
-			<section className="content double">
-				<div className="content-container full-width">
-					<img src="/images/listings.png" />
-					<h2>This is a two column page that can be used as a single column.</h2>
-				</div>
-				<div className="content-container flex">
-						<div className="text">
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mauris massa, auctor vitae lorem lacinia, scelerisque lacinia est. Cras sed ipsum non mauris pharetra interdum quis eu turpis. Curabitur volutpat magna est, quis imperdiet mi placerat vitae. Aenean at libero viverra, cursus eros quis, condimentum urna. In non aliquam justo. Nam pulvinar sapien efficitur tortor sagittis venenatis. Cras porta elit eget pulvinar sollicitudin. Cras sollicitudin mattis egestas. Praesent consequat commodo justo, tincidunt imperdiet ex vulputate id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam lobortis ornare cursus. Ut dignissim eros ac dolor tincidunt bibendum. Proin rhoncus ipsum nec tellus vehicula faucibus. Proin pellentesque purus dolor, in tempus nisi gravida non. Sed interdum sem eget ex ultrices commodo. Proin est urna, tristique a dapibus nec, malesuada sollicitudin sapien.</p>
-	
-	<p>Nam et purus non nisi vulputate eleifend in sed orci. Nunc porta ac velit id hendrerit. Phasellus vel orci nec eros rutrum volutpat a eu odio. Maecenas gravida urna ut porttitor euismod. Nulla facilisi. Duis neque augue, tincidunt non tincidunt vel, tempus at lorem. Vivamus condimentum convallis justo ut porta. Vivamus dapibus lorem libero, eget volutpat urna fringilla non. Donec iaculis ac tellus ac aliquet. </p>
-						</div>
-						<div className="text">
-							<h2>This is a two column page that can be used as a single column.</h2>
-	
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mauris massa, auctor vitae lorem lacinia, scelerisque lacinia est. Cras sed ipsum non mauris pharetra interdum quis eu turpis. Curabitur volutpat magna est, quis imperdiet mi placerat vitae. Aenean at libero viverra, cursus eros quis, condimentum urna. In non aliquam justo. Nam pulvinar sapien efficitur tortor sagittis venenatis. Cras porta elit eget pulvinar sollicitudin. Cras sollicitudin mattis egestas. Praesent consequat commodo justo, tincidunt imperdiet ex vulputate id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam lobortis ornare cursus. Ut dignissim eros ac dolor tincidunt bibendum. Proin rhoncus ipsum nec tellus vehicula faucibus. Proin pellentesque purus dolor, in tempus nisi gravida non. Sed interdum sem eget ex ultrices commodo. Proin est urna, tristique a dapibus nec, malesuada sollicitudin sapien.</p>
-	
-	<p>Nam et purus non nisi vulputate eleifend in sed orci. Nunc porta ac velit id hendrerit. Phasellus vel orci nec eros rutrum volutpat a eu odio. Maecenas gravida urna ut porttitor euismod. Nulla facilisi. Duis neque augue, tincidunt non tincidunt vel, tempus at lorem. Vivamus condimentum convallis justo ut porta. Vivamus dapibus lorem libero, eget volutpat urna fringilla non. Donec iaculis ac tellus ac aliquet. </p>
-						</div>
-				</div>
-			</section>
-			
-			<div className="side-area content-container">
-				<img src="/images/standard-image.jpg" />
-      		</div>
-      		
-      		
-			<section className="content double">
-				<div className="content-container full-width">
-					<img src="/images/listings.png" />
-					<h2>This is a two column page that can be used as a single column.</h2>
-				</div>
-				<div className="content-container flex">
-						<div className="text">
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In mauris massa, auctor vitae lorem lacinia, scelerisque lacinia est. Cras sed ipsum non mauris pharetra interdum quis eu turpis. Curabitur volutpat magna est, quis imperdiet mi placerat vitae. Aenean at libero viverra, cursus eros quis, condimentum urna. In non aliquam justo. Nam pulvinar sapien efficitur tortor sagittis venenatis. Cras porta elit eget pulvinar sollicitudin. Cras sollicitudin mattis egestas. Praesent consequat commodo justo, tincidunt imperdiet ex vulputate id. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam lobortis ornare cursus. Ut dignissim eros ac dolor tincidunt bibendum. Proin rhoncus ipsum nec tellus vehicula faucibus. Proin pellentesque purus dolor, in tempus nisi gravida non. Sed interdum sem eget ex ultrices commodo. Proin est urna, tristique a dapibus nec, malesuada sollicitudin sapien.</p>
-	
-	<p>Nam et purus non nisi vulputate eleifend in sed orci. Nunc porta ac velit id hendrerit. Phasellus vel orci nec eros rutrum volutpat a eu odio. Maecenas gravida urna ut porttitor euismod. Nulla facilisi. Duis neque augue, tincidunt non tincidunt vel, tempus at lorem. Vivamus condimentum convallis justo ut porta. Vivamus dapibus lorem libero, eget volutpat urna fringilla non. Donec iaculis ac tellus ac aliquet. </p>
-						</div>
-						<div>
-							<img src="/images/content-image.jpg" />
-							<div className="caption">Caption goes here</div>
-						</div>
-				</div>
-			</section>
 
-			
-		</div>
+    return (
+      <div className="standard-page">
+        <Helmet title="Avnu - Standard Page" />
+        <Hero
+          mainTitle={mainTitle}
+          introText={introText}
+          imgSrc="/images/header.jpg"
+          icon="/images/clock.svg"
+          headline={pageHeading}
+        />
+
+        {this.state.data &&
+          this.state.data.contentBlocks.map((block, i) => {
+            console.log("block", block);
+
+            if (i < 1) {
+              let blockBody = getMarkup(block.fields.blockBody);
+              return (
+                <Zoom key={i}>
+                  <section key={i} className="content double">
+                    <div className="content-container flex">
+                      <div>
+                        <img src="/images/content-image.jpg" />
+                      </div>
+                      <div className="text">{blockBody}</div>
+                    </div>
+                  </section>
+                </Zoom>
+              );
+            }
+          })}
+      </div>
     );
   }
 }
 
-export default StandardPage;
+export default withRouter(StandardPage);
