b9e5216b9e0c6d12792c6b2c56485cfcc48015d7
James <james@ljx.com.au>
2018-07-16 14:54:28 +1000

    hero image now done

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
-import React, { Component, createElement } from 'react';
-import Helmet from 'react-helmet';
-import { createClient } from 'contentful';
-import PostLink from './PostLink';
+import React, { Component, createElement } from "react";
+import Helmet from "react-helmet";
+import { createClient } from "contentful";
+import PostLink from "./PostLink";
 import Slider from "react-slick";
-import Zoom from 'react-reveal/Zoom'; 
-import marksy from 'marksy'; 
-import InViewMonitor from 'react-inview-monitor';
+import Zoom from "react-reveal/Zoom";
+import marksy from "marksy";
+import InViewMonitor from "react-inview-monitor";
 
 const getMarkup = field => {
   if (!field) return null;
@@ -28,64 +28,74 @@ class Home extends Component {
       space: process.env.REACT_APP_SPACE_ID,
       accessToken: process.env.REACT_APP_ACCESS_TOKEN
     });
-	
-	
-	client
-	  // use getEntries because it does link resolution
-	  .getEntries({
-	    content_type: 'homepage',
-	    include: 3
-	  })
-	  .then(response => {
-	    // extract the data from the response array
-	    return response.items[0].fields;
-	  })
-	  .then(fields => {
-		  console.log(fields);
-	    this.setState({
-		  pageData: fields,
+
+    client
+      // use getEntries because it does link resolution
+      .getEntries({
+        content_type: "homepage",
+        include: 3
+      })
+      .then(response => {
+        // extract the data from the response array
+        return response.items[0].fields;
+      })
+      .then(fields => {
+        console.log(fields);
+        this.setState({
+          pageData: fields,
           heroItems: fields.heroImageScrollerImages
         });
-	  })
-	  .catch(console.error);
-	
+      })
+      .catch(console.error);
   }
-  
+
   render() {
     let imgSrc = null,
-    	imgTileSrc = null;
+      imgTileSrc = null;
     let heroImageValuePropGreeting,
-    	heroImageValuePropHeading,
-    	heroImageValuePropBody,
-    	missionStatement,
-    	missionStatementLink,
-    	missionStatementLinkText,
-    	primaryQuote;
-    
+      heroImageValuePropHeading,
+      heroImageValuePropBody,
+      missionStatement,
+      missionStatementLink,
+      missionStatementLinkText,
+      primaryQuote,
+      primaryHeroImage;
+
     // Slider Settings.
     var settings = {
-      dots: true, 
+      dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1
     };
-    
-	if (this.state.pageData) {
-		heroImageValuePropGreeting = getMarkup(this.state.pageData.heroImageValuePropGreeting);
-		heroImageValuePropHeading = getMarkup(this.state.pageData.heroImageValuePropHeading);
-		heroImageValuePropBody = getMarkup(this.state.pageData.heroImageValuePropBody);
-		missionStatement = getMarkup(this.state.pageData.missionStatement);
-		missionStatementLink = this.state.pageData.missionStatementLinkTo;
-		missionStatementLinkText = this.state.pageData.missionStatementLinkToText;
-		primaryQuote = getMarkup(this.state.pageData.primaryQuote.fields.quoteBody);
-	}
-    
+
+    if (this.state.pageData) {
+      heroImageValuePropGreeting = getMarkup(
+        this.state.pageData.heroImageValuePropGreeting
+      );
+      heroImageValuePropHeading = getMarkup(
+        this.state.pageData.heroImageValuePropHeading
+      );
+      heroImageValuePropBody = getMarkup(
+        this.state.pageData.heroImageValuePropBody
+      );
+      missionStatement = getMarkup(this.state.pageData.missionStatement);
+      missionStatementLink = this.state.pageData.missionStatementLinkTo;
+      missionStatementLinkText = this.state.pageData.missionStatementLinkToText;
+      primaryQuote = getMarkup(
+        this.state.pageData.primaryQuote.fields.quoteBody
+      );
+    }
+    if (this.state.heroItems) {
+      console.log(this.state.heroItems);
+      primaryHeroImage = this.state.heroItems[0].fields.file.url;
+    }
     return (
-	    <div>
-	      <div className="hero-slider">
-	      	<Helmet title="Avnu - Properties" />
-		  	{/*<Slider {...settings}>
+      <div>
+        <div className="hero-slider">
+          <Helmet title="Avnu - Properties" />
+          {/*<Slider {...settings}>
 	       		{this.state.heroItems && this.state.heroItems.map((slide, i) => {     
 		       		 imgSrc = slide.fields.file.url;
 		             return (
@@ -106,85 +116,116 @@ class Home extends Component {
 		            );             
 		        })}
 		    </Slider>*/}
-              	<div className="slider-bg" style={{backgroundImage: `url(/images/hero.jpg)`}}>
-              	<img src="/images/hero.jpg" />
-              		<div className="hero-text">
-	              		<div className="content-container">
-		              		<div className="welcome">
-		              			{heroImageValuePropGreeting}
-		              			{heroImageValuePropHeading}
-		              			{heroImageValuePropBody}
-		              		</div>
-		              	</div>
-	              	</div>
-              	</div>
-	      </div>
-	      
-	      <div className="mission-statement">
-	      		<div className="content">
-	      			<img src="/images/clock.svg" className="clock" alt="logo" />
-	      			{missionStatement}
-			  		<a href={missionStatementLink} className="with-arrow">{missionStatementLinkText}</a>
-	      		</div>
-	      </div>
-	      
-	      <div className="secondary-tiles">
-	      	<div className="content-container">
-	      		{this.state.pageData && this.state.pageData.secondaryValuePropTiles.map((tiles, i) => {     
-		       		 imgTileSrc = tiles.fields.tileImage.fields.file.url;
-		             return (
-		              	<div key={i} className="content last">
-			      			<img src={imgTileSrc} />
-			      			<div className="info">
-			      				<h3>{tiles.fields.tileTitle}</h3>
-			      				<p>{tiles.fields.tileBody}</p>
-			      				<a href={tiles.fields.tileLinkTo} className="with-arrow">{tiles.fields.tileLinkToText}</a>
-			      			</div>
-			      		</div>
-		            );             
-		        })}
-	      	</div>
-	      </div>
-	      
-	      <div className="quoting-area">
-	      		<div className="grey"></div>
-	      		 <InViewMonitor
-				  classNameNotInView='vis-hidden'
-				  classNameInView='animated slideInRight content-container'
-				>
-		      		<div className="the-quote">
-		      			<img src="https://images.ctfassets.net/dkcrc82u6zt9/GCYZPz8aAeo8Uw4miyIIo/06f487a622bb6d2d3681c87d0b3d1bd0/man.png" />
-		      			{primaryQuote}
-		      		</div>
-	      		</InViewMonitor>
-	      		<div className="white"></div>
-	      </div>
-	      
-	      
-	       <div className="three-tiles">
-	       		<div className="content-container">
-	       		<Zoom>
-		      	<div className="row">
-		      	
-			      	{this.state.pageData && this.state.pageData.standardContentBlockTiles.map((contentBlocks, i) => {     
-			       		 imgTileSrc = contentBlocks.fields.homePageDisplayTile.fields.tileImage.fields.file.url;
-			             return (
-			              	<div key={i} className="content">
-				      			<img src={imgTileSrc} />
-				      			<div className="info">
-				      				<h3>{contentBlocks.fields.homePageDisplayTile.fields.tileTitle}</h3>
-				      				<p>{contentBlocks.fields.homePageDisplayTile.fields.tileBody}</p>
-				      				<a href={contentBlocks.fields.homePageDisplayTile.fields.tileLinkTo} className="with-arrow">{contentBlocks.fields.homePageDisplayTile.fields.tileLinkToText}</a>
-				      			</div>
-				      		</div>
-			            );             
-			        })}
-		      	
-		      	</div>
-		      	</Zoom>
-		      	</div>
-	      </div>
-		</div>
+          <div
+            className="slider-bg"
+            style={{ backgroundImage: `url(${primaryHeroImage})` }}
+          >
+            <img src={primaryHeroImage} />
+            <div className="hero-text">
+              <div className="content-container">
+                <div className="welcome">
+                  {heroImageValuePropGreeting}
+                  {heroImageValuePropHeading}
+                  {heroImageValuePropBody}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mission-statement">
+          <div className="content">
+            <img src="/images/clock.svg" className="clock" alt="logo" />
+            {missionStatement}
+            <a href={missionStatementLink} className="with-arrow">
+              {missionStatementLinkText}
+            </a>
+          </div>
+        </div>
+
+        <div className="secondary-tiles">
+          <div className="content-container">
+            {this.state.pageData &&
+              this.state.pageData.secondaryValuePropTiles.map((tiles, i) => {
+                imgTileSrc = tiles.fields.tileImage.fields.file.url;
+                return (
+                  <div key={i} className="content last">
+                    <img src={imgTileSrc} />
+                    <div className="info">
+                      <h3>{tiles.fields.tileTitle}</h3>
+                      <p>{tiles.fields.tileBody}</p>
+                      <a href={tiles.fields.tileLinkTo} className="with-arrow">
+                        {tiles.fields.tileLinkToText}
+                      </a>
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+
+        <div className="quoting-area">
+          <div className="grey" />
+          <InViewMonitor
+            classNameNotInView="vis-hidden"
+            classNameInView="animated slideInRight content-container"
+          >
+            <div className="the-quote">
+              <img src="https://images.ctfassets.net/dkcrc82u6zt9/GCYZPz8aAeo8Uw4miyIIo/06f487a622bb6d2d3681c87d0b3d1bd0/man.png" />
+              {primaryQuote}
+            </div>
+          </InViewMonitor>
+          <div className="white" />
+        </div>
+
+        <div className="three-tiles">
+          <div className="content-container">
+            <Zoom>
+              <div className="row">
+                {this.state.pageData &&
+                  this.state.pageData.standardContentBlockTiles.map(
+                    (contentBlocks, i) => {
+                      imgTileSrc =
+                        contentBlocks.fields.homePageDisplayTile.fields
+                          .tileImage.fields.file.url;
+                      return (
+                        <div key={i} className="content">
+                          <img src={imgTileSrc} />
+                          <div className="info">
+                            <h3>
+                              {
+                                contentBlocks.fields.homePageDisplayTile.fields
+                                  .tileTitle
+                              }
+                            </h3>
+                            <p>
+                              {
+                                contentBlocks.fields.homePageDisplayTile.fields
+                                  .tileBody
+                              }
+                            </p>
+                            <a
+                              href={
+                                contentBlocks.fields.homePageDisplayTile.fields
+                                  .tileLinkTo
+                              }
+                              className="with-arrow"
+                            >
+                              {
+                                contentBlocks.fields.homePageDisplayTile.fields
+                                  .tileLinkToText
+                              }
+                            </a>
+                          </div>
+                        </div>
+                      );
+                    }
+                  )}
+              </div>
+            </Zoom>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/StandardPage.js
+++ src/StandardPage.js
@@ -1,9 +1,9 @@
-import React, { Component, createElement } from 'react';
-import Helmet from 'react-helmet';
-import { createClient } from 'contentful';
-import Hero from './Hero';
-import Zoom from 'react-reveal/Zoom';
-import marksy from 'marksy'; 
+import React, { Component, createElement } from "react";
+import Helmet from "react-helmet";
+import { createClient } from "contentful";
+import Hero from "./Hero";
+import Zoom from "react-reveal/Zoom";
+import marksy from "marksy";
 
 const getMarkup = field => {
   if (!field) return null;
@@ -28,12 +28,12 @@ class StandardPage extends Component {
       space: process.env.REACT_APP_SPACE_ID,
       accessToken: process.env.REACT_APP_ACCESS_TOKEN
     });
-
+    console.log(this.props.match.params.id);
     client
       // use getEntries because it does link resolution
       .getEntries({
-	    content_type: 'standardContentPages',
-        'fields.slug[in]': this.props.match.params.id,
+        content_type: "standardContentPages",
+        "fields.slug[in]": this.props.match.params.id,
         include: 3
       })
       .then(response => {
@@ -48,95 +48,98 @@ class StandardPage extends Component {
       })
       .catch(console.error);
   }
-  
+
   componentDidMount() {
-	  document.body.classList.add('standard-page-content');
+    document.body.classList.add("standard-page-content");
   }
 
   render() {
     let mainTitle = null,
-    	introText = null,
-    	pageHeading = null,
-    	heroData = null,
-    	headline = null,
-    	heroImage = null,
-    	pageData = null,
-    	contentBloks;
-		
-	 if (this.state.data) {
+      introText = null,
+      pageHeading = null,
+      heroData = null,
+      headline = null,
+      heroImage = null,
+      pageData = null,
+      contentBloks;
+
+    if (this.state.data) {
       heroData = this.state.data;
       pageData = this.state.data;
-	  mainTitle = heroData.heroImageBody;
-	  introText = heroData.heroImageHeading;
-	  heroImage = heroData.heroImage.fields.file.url;
-	  headline = heroData.pageHeading;
-	  contentBloks = pageData.contentBloks;
+      mainTitle = heroData.heroImageHeading;
+      introText = heroData.heroImageBody;
+      heroImage = heroData.heroImage.fields.file.url;
+      headline = heroData.pageHeading;
+      contentBloks = pageData.contentBloks;
     }
-    
+
     return (
-	    <div className="standard-page">
-	    	<Helmet title={"Avnu - " + mainTitle} />
-			<Hero 
-				mainTitle={mainTitle}
-				introText={introText}
-				imgSrc={heroImage} 
-				icon="/images/clock.svg"
-				headline={headline}
-			/>
-			
-			
-			{contentBloks && contentBloks.map((block, i) => {   
-		       		 let blockBody = getMarkup(block.fields.blockBody);
-		       		 let blockImage = block.fields.blockImage;
-		       		 let blockTitle = block.fields.blockTitle;
-			   		 
-			   		 
-			   		 if(block.fields.image) {
-				   		 return (
-					   		 <div className="side-area content-container">
-								<img src={block.fields.image.fields.file.url} />
-				      		</div>
-				   		 );
-			   		 } else if(block.fields.quoteBody) {
-			       		 return (
-							 <div className="quote-area content-container">
-								<div className="the-quote">
-					      			<img src="https://images.ctfassets.net/dkcrc82u6zt9/GCYZPz8aAeo8Uw4miyIIo/06f487a622bb6d2d3681c87d0b3d1bd0/man.png" />
-					      			<p>{block.fields.quoteBody}</p>
-					      		</div>
-					      	   </div>
-			            ); 
-		       		 } else {
-			             return (
-							 <section key={i} className="content double">
-							 	{blockTitle ?
-									[
-									<div className="content-container full-width">
-										<img src="/images/listings.png" />
-										<h2>{blockTitle}</h2>
-									</div>
-									] : null
-								}
-								<div className="content-container flex">
-								{blockImage ?
-									[
-									<div>
-										<img src={blockImage.fields.file.url} />
-									</div>
-									] : null
-								}
-									<div className="text">
-										{blockBody}
-									</div>
-								</div>
-							 </section>
-			            );   
-		            }  
-		             
-	        })}
-			
-			
-		</div>
+      <div className="standard-page">
+        <Helmet title={"Avnu - " + mainTitle} />
+        <Hero
+          mainTitle={mainTitle}
+          introText={introText}
+          imgSrc={heroImage}
+          icon="/images/clock.svg"
+          headline={headline}
+        />
+
+        {contentBloks &&
+          contentBloks.map((block, i) => {
+            let blockBody = getMarkup(block.fields.blockBody);
+            let blockImage = block.fields.blockImage;
+            let blockTitle = block.fields.blockTitle;
+
+            let blockIconography = block.fields.iconography;
+            console.log("blockIconography", blockIconography);
+            if (block.fields.image) {
+              return (
+                <div className="side-area content-container">
+                  <img src={block.fields.image.fields.file.url} />
+                </div>
+              );
+            } else if (block.fields.quoteBody) {
+              return (
+                <div className="quote-area content-container">
+                  <div className="the-quote">
+                    <img src="https://images.ctfassets.net/dkcrc82u6zt9/GCYZPz8aAeo8Uw4miyIIo/06f487a622bb6d2d3681c87d0b3d1bd0/man.png" />
+                    <p>{block.fields.quoteBody}</p>
+                  </div>
+                </div>
+              );
+            } else {
+              return (
+                <section key={i} className="content double">
+                  {blockTitle
+                    ? [
+                        <div className="content-container full-width">
+                          {blockIconography
+                            ? [
+                                <img
+                                  className="iconography"
+                                  src={blockIconography.fields.file.url}
+                                />
+                              ]
+                            : null}
+                          <h2>{blockTitle}</h2>
+                        </div>
+                      ]
+                    : null}
+                  <div className="content-container flex">
+                    {blockImage
+                      ? [
+                          <div>
+                            <img src={blockImage.fields.file.url} />
+                          </div>
+                        ]
+                      : null}
+                    <div className="text">{blockBody}</div>
+                  </div>
+                </section>
+              );
+            }
+          })}
+      </div>
     );
   }
 }
